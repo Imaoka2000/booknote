333bc334cd4475b5a2a29677a8eec0725d2c7b6b
Imaoka2000 <beta200010@gmail.com>
2025-06-17 01:35:58 +0900

    レビュー投稿作成

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root "reviews#index"
+  resources :reviews, only: [ :index, :new, :create, :show ]
 end
