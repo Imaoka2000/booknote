d3c1deecdaca8f4b9d4123cc75c094b4db354d54
Imaoka2000 <beta200010@gmail.com>
2025-06-17 17:14:39 +0900

    WIP

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_12_165315) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_17_073428) do
   create_table "reviews", force: :cascade do |t|
     t.string "isbn"
     t.text "content"
@@ -28,6 +28,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_12_165315) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
