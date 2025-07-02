53f33f3a99c22552448c9f7809ae4cb0620f8de9
Imaoka2000 <beta200010@gmail.com>
2025-07-02 15:40:59 +0900

    WIP

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -24,8 +24,8 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
-    review = Review.find(params[:id])
-    review.destroy
+    @review = Review.find(params[:id])
+    @review.destroy
     redirect_to reviews_path, notice: "レビューを削除しました"
   end
 
