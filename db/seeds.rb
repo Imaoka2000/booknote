a31ffaa533bab5db3ac692aa21ab62104f1aac49
Imaoka2000 <beta200010@gmail.com>
2025-07-02 11:21:57 +0900

    chore: テスト用のサンプルデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# ユーザー作成
+user = User.create!(
+  username: "テストユーザー1",
+  email: "test1@example.com",
+  password: "password",
+  password_confirmation: "password"
+)
+
+user = User.create!(
+  username: "テストユーザー2",
+  email: "test2@example.com",
+  password: "password",
+  password_confirmation: "password"
+)
+
+# レビュー作成（レビューがUserに紐づいている前提）
+Review.create!(
+  user_id: 1,
+  content: "これはテスト用のレビューです。",
+  isbn: "9784088725093"
+)
+
+Review.create!(
+  user_id: 2,
+  content: "これはテスト用のレビューです。",
+  isbn: "9784088725444"
+)
