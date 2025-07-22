f46c20567cd997cac87b9920e3b11ad12137dd4f
Imaoka2000 <beta200010@gmail.com>
2025-07-22 14:23:35 +0900

    bookモデル作成

---
+++ db/migrate/20250722041722_create_books.rb
@@ -0,0 +1,10 @@
+class CreateBooks < ActiveRecord::Migration[8.0]
+  def change
+    create_table :books do |t|
+      t.string :isbn
+
+      t.timestamps
+    end
+    add_index :books, :isbn
+  end
+end
