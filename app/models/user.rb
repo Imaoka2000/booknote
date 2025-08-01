0463e14cc86588aeec872efef60cdd242e82f7e4
Imaoka2000 <beta200010@gmail.com>
2025-08-01 12:04:11 +0900

    feat: followモデルとコントローラーの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :reviews, dependent: :destroy
   has_many :review_likes, dependent: :destroy
+  has_many :followers, class_name: "Follow", foreign_key: "followed_id", dependent: :destroy
+  has_many :followeds, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
   has_one_attached :profile_image
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_31_105444) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_01_024045) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -44,6 +44,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_31_105444) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["isbn"], name: "index_books_on_isbn", unique: true
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "review_likes", force: :cascade do |t|
